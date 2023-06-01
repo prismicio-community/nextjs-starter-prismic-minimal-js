2d581aaa0c1a7725dc77a2618577d5dceafa1d69
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-01 13:33:11 -1000

    fix: replace `@prismicio/helpers` with `@prismicio/client`

--- src/pages/[uid].js
+++ src/pages/[uid].js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { SliceZone } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
-import * as prismicH from "@prismicio/helpers";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
@@ -13,7 +12,7 @@ export default function Page({ page }) {
   return (
     <main>
       <Head>
-        <title>{prismicH.asText(page.data.title)}</title>
+        <title>{prismic.asText(page.data.title)}</title>
       </Head>
       <SliceZone slices={page.data.slices} components={components} />
     </main>
@@ -47,7 +46,7 @@ export async function getStaticPaths() {
    */
   return {
     paths: pages.map((page) => {
-      return prismicH.asLink(page);
+      return prismic.asLink(page);
     }),
     fallback: false,
   };
