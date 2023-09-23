defd4a9e1757761e8a1f07e389e89a12d43684e4
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-09-22 14:35:16 -1000

    feat: update preview routes

--- src/app/api/exit-preview/route.js
+++ src/app/api/exit-preview/route.js
@@ -4,6 +4,6 @@ import { exitPreview } from "@prismicio/next";
  * This endpoint exits a preview session.
  */
 
-export async function GET() {
-  return await exitPreview();
+export function GET() {
+  return exitPreview();
 }
